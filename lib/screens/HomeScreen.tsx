031932a038ee7553603b700cb658970c78223a52
jia-son <116625758+jia-son@users.noreply.github.com>
2023-12-20 18:48:19 +0900

    아이콘 추가

--- lib/screens/HomeScreen.tsx
+++ lib/screens/HomeScreen.tsx
@@ -1,11 +1,75 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/Fontisto';
 
 export function HomeScreen() {
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    var interval: any;
+
+    if (isActive) {
+      interval = setInterval(() => {
+        if (seconds !== 0) {
+          setSeconds(prevSeconds => prevSeconds - 1);
+        } else {
+          handleReset();
+        }
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, seconds]);
+
+  const formattedTime = () => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${String(minutes).padStart(2, '0')}:${String(
+      remainingSeconds,
+    ).padStart(2, '0')}`;
+  };
+
+  const timeSetting = () => {
+    setSeconds(2700);
+  };
+
+  const handleIsActive = () => {
+    setIsActive(!isActive);
+  };
+
+  const handleReset = () => {
+    timeSetting();
+    setIsActive(false);
+  };
+
   return (
     <View style={styles.mainContainer}>
       <View style={styles.timer}>
-        <Text style={styles.text}>홈 스크린</Text>
+        <Text style={styles.text}>{formattedTime()}</Text>
+        <TouchableOpacity onPress={() => timeSetting()}>
+          <Text style={{...styles.text, fontSize: 18, paddingTop: 50}}>
+            시간 선택
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.btnContainer}>
+        <TouchableOpacity onPress={handleIsActive}>
+          <Text>
+            {isActive ? (
+              <Icon name="pause" size={30} color={'white'} />
+            ) : (
+              <Icon name="play" size={30} color={'white'} />
+            )}
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handleReset}>
+          <Text>
+            <Icon name="stop" size={30} color={'white'} />
+          </Text>
+        </TouchableOpacity>
       </View>
       <View style={styles.menuBar}>
         <Text style={styles.menuItem}>타이머</Text>
@@ -20,7 +84,7 @@ export function HomeScreen() {
 const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
-    backgroundColor: 'grey',
+    backgroundColor: '#252321',
     justifyContent: 'center',
   },
   text: {
@@ -29,7 +93,7 @@ const styles = StyleSheet.create({
     fontWeight: '600',
   },
   timer: {
-    flex: 9,
+    flex: 7,
     justifyContent: 'center',
     alignItems: 'center',
   },
@@ -43,5 +107,12 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 20,
     fontWeight: '500',
+  },
+  btnContainer: {
+    flex: 2,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    paddingBottom: 100,
   },
 });
